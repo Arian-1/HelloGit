328aa83493fd78a5867942a18e0376bdda3f32d0
Arian-1 <al062231@uacam.mx>
2024-02-02 23:31:15 -0500

    correcion conflicto2

--- hellogit3.java
+++ hellogit3.java
@@ -1,9 +1,5 @@
 public class hellogit3 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("ola v login");
-=======
         System.out.println("ola v3");
->>>>>>> main
     }
 }
